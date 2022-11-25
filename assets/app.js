d59e5463367301994821fb5babc676a7871c2a17
naailah jugon <jugonnaailah@gmail.com>
2022-11-25 12:18:18 +0100

    created defaultcontroller with index.html.twig template

---
+++ assets/app.js
@@ -0,0 +1,37 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+import * as bootstrap from 'bootstrap'
+
+import logoPath from '../assets/images/path_to_image.png';
+
+let html = `<img src="${logoPath}" alt="ACME logo">`;
+
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
