b53570dea07a21985722cd577fc56d909fed7561
naailah jugon <jugonnaailah@gmail.com>
2022-11-29 01:20:01 +0100

    added image poster

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,20 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
             return $this->render('program/index.html.twig', [
-                'website' => 'Wild Series',
+                'programs' => $programs,
         ]);
 
         
     }
-
-    #[Route('/program/{id}', methods: ['GET'], requirements: ['page'=>'\d+'], name: 'program_show')] 
-    public function show(int $id = 4): Response
+    
+    #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
+    // #[Route('/program/{id}', methods: ['GET'], requirements: ['page'=>'\d+'], name: 'program_show')] //
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
             return $this->render('program/_show.html.twig', [
-                'id' => '4',
+                'program' => $program,
         ]);
 
         
